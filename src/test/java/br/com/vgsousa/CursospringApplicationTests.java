5f6a523ea212547a5cae21d7d196657e75f84eeb
Vitor Sousa <vguedes.sousa@gmail.com>
2018-10-15 17:43:31 -0300

    Inicio do Projeto

---
+++ src/test/java/br/com/vgsousa/CursospringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vgsousa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursospringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
