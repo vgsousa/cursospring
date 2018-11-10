c4be7fee8288e6d28c867d52608dbbb9450314f4
Vitor Sousa <vguedes.sousa@gmail.com>
2018-11-10 20:42:56 -0200

    Lista categorias com DTO

--- src/main/java/br/com/vgsousa/resources/CategoriaResource.java
+++ src/main/java/br/com/vgsousa/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.vgsousa.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.vgsousa.domain.Categoria;
+import br.com.vgsousa.dto.CategoriaDTO;
 import br.com.vgsousa.services.CategoriaService;
 
 @RestController
@@ -20,6 +23,15 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
--- src/main/java/br/com/vgsousa/services/CategoriaService.java
+++ src/main/java/br/com/vgsousa/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.vgsousa.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repositorio;
 
+	
+	public List<Categoria> findAll() {
+		return repositorio.findAll();
+	}
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repositorio.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
