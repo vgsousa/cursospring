d503c0a049ea5cf76c73e252f0cb69734f6209a7
Vitor G <vguedes.sousa@gmail.com>
2019-04-20 18:21:56 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/vgsousa/config/DevConfig.java
@@ -0,0 +1,30 @@
+package br.com.vgsousa.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.vgsousa.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy))
+			dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+}
